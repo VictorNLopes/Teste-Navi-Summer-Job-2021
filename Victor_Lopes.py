6200125716abb876297d2bad65b9127cbbceeb07
VictorNLopes <69806937+VictorNLopes@users.noreply.github.com>
2020-12-19 14:52:34 -0300

    Update Victor_Lopes.py

--- Victor_Lopes.py
+++ Victor_Lopes.py
@@ -1,35 +1,34 @@
 import numpy as np
 from numba import jit
 
-
 # 1
 inicio = 1
 fim = int(5e6)
 
 
 @jit(nopython=True, fastmath={'reassoc', 'nsz'}, parallel=True)
-def f(start, end):
-    
-    x = np.arange(start, end+1)
-    x = len(x[(x % 2 == 0.) & (x % 49 == 0.) & (x % 37 == 0.)])
+def f(start: int, end: int):
+	x = np.arange(start, end + 1)
+	x = len(x[(x % 2 == 0.) & (x % 49 == 0.) & (x % 37 == 0.)])
+	
+	return x
 
-    return x
-    
-print('Existem ' + str(rapido(inicio, fim)) + ' números que satisfazem as três condições.')
+
+print('Existem ' + str(f(inicio, fim)) + ' números que satisfazem as três condições.')
 
 
 # 2
-def g(tamanho, precisao=2):
-    
-    x = []
-    for i in range(tamanho):
-        if i % 2 == 0:
-            x.append(3**i + 7*np.math.factorial(i))
-        else:
-            x.append(2**i + 4*np.log(i))
-            
-    x = np.asarray(x)
-    return x, np.argmax(x), np.around(np.mean(x), precisao)
+def g(tamanho: int, precisao=2):
+	
+	x = []
+	for i in range(tamanho):
+		if i % 2 == 0:
+			x.append(3**i + 7*np.math.factorial(i))
+		else:
+			x.append(2**i + 4*np.log(i))
+	
+	x = np.asarray(x)
+	return x, np.argmax(x), np.around(np.mean(x), precisao)
 
 
 a, b, c = g(10)
@@ -39,21 +38,14 @@ print('O valor médio do vetor é: ' + str(c) + '.')
 # 3
 alunos = {}
 for n in range(5):
-    nome  = input('Nome do aluno ' + str(n+1) + ': ')
-    nota  = input('Nota do aluno ' + str(n+1) + ': ')
-    alunos[nome] = float(nota)
+	nome = input('Nome do aluno ' + str(n + 1) + ': ')
+	nota = input('Nota do aluno ' + str(n + 1) + ': ')
+	alunos[nome] = float(nota)
 
-x = max(alunos.values())
+maior_nota = max(alunos.values())
 for m, n in alunos.items():
-    if n == x:
-        print('O aluno com a maior nota se chama ' + m + ' e sua nota é: ' + str(n) + '.')
-        
-
-
-
-
-
-
+	if n == maior_nota:
+		print('O aluno com a maior nota se chama ' + m + ' e sua nota é: ' + str(n) + '.')
 
 
 
